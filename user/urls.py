dfd607a7c9a0c9b09c19dbda9111694c66cac7c7
harry <dammykuye@gmail.com>
2022-08-08 07:09:53 +0100

    update migrations

--- user/urls.py
+++ user/urls.py
@@ -1,3 +1,5 @@
+from djoser import views
+from rest_framework_nested import routers
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
@@ -5,13 +7,14 @@ from . import views
 from djoser.urls import urlpatterns as url
 app_name = 'user'
 
+router = routers.DefaultRouter()
 
 urlpatterns = [
    path('me/add-following/', views.Following.as_view(), name='following'),
    path('<int:pk>/', views.UserProfile.as_view(), name='profile'),
    path('<int:pk>/followers/', views.getFollowers.as_view(), name='followers'),
    path('<int:pk>/following/', views.getFollowing.as_view(), name='following'),
-   path('', include('djoser.urls')),
+   router.register("", views.UserViewSet),
    path('', include('djoser.urls.jwt'))
 
 ]
