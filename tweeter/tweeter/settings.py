a2d53c25ed0d937ef09190c77d2b6f2f654175a8
harry <dammykuye@gmail.com>
2022-08-08 07:03:33 +0100

    aws branch

--- tweeter/tweeter/settings.py
+++ tweeter/tweeter/settings.py
@@ -76,6 +76,11 @@ TEMPLATES = [
 ]
 
 SPECTACULAR_SETTINGS = {
+    'TITLE': 'Your Project API',
+    'DESCRIPTION': 'Your project description',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+    # OTHER SETTINGS
     'SWAGGER_UI_DIST': 'SIDECAR',
     'SWAGGER_UI_FAVICON_HREF': 'SIDECAR',
     'REDOC_DIST': 'SIDECAR',
