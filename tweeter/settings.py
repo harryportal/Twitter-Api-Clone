3138b4213b1b741ce45dbc32d9dd7b5e9f2c3199
harry <dammykuye@gmail.com>
2022-08-08 11:40:21 +0100

    commit

--- tweeter/settings.py
+++ tweeter/settings.py
@@ -81,8 +81,7 @@ TEMPLATES = [
 
 SPECTACULAR_SETTINGS = {
     'TITLE': 'TWIITER CLONE API',
-    'DESCRIPTION': "This is a simple clone of Twiiter API built with Django Rest Framework(Use Desktop view if you're"
-                   " viewing with mobile!)",
+    'DESCRIPTION': "This is a simple clone of Twiiter API built with Django Rest Framework(Use Desktop view if you're viewing with mobile!)",
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
     'SWAGGER_UI_DIST': 'SIDECAR',
