aeefc4b82b5a7efffee0fda71802060386dc9647
harry <dammykuye@gmail.com>
2022-08-20 02:49:09 +0100

    test mailing

--- tweeter/tweeter/settings.py
+++ tweeter/tweeter/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
