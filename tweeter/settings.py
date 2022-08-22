92257f7468a3eeda2bc87792b04ad7ab655c0496
harry <dammykuye@gmail.com>
2022-08-22 13:57:13 +0100

    switch to whitenoise

--- tweeter/settings.py
+++ tweeter/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
